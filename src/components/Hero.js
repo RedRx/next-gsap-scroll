8aa48503dcd3cb9fba0cfe9e69cac6973a356030
BEARYDEV <teerapat.tech@gmail.com>
2023-11-03 10:41:45 +0700

    me name

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,8 +3,8 @@ import React from "react";
 function Hero() {
   return (
     <header className="hero__header">
-      <h1>John Doe</h1>
-      <h2>Frontend Developer</h2>
+      <h1>Beary Dev</h1>
+      <h2>Software Developer</h2>
     </header>
   );
 }
